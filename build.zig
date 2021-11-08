ff09c47a21351f8865d550925d1cc69e51ca4eef
Jay Petacat <jay@jayschwa.net>
2021-11-07 21:37:33 -0500

    Remove Zig 0.8 support from build.zig

--- build.zig
+++ build.zig
@@ -15,25 +15,15 @@ pub fn build(b: *Builder) !void {
     coff_exe.disable_stack_probing = true;
     coff_exe.addPackagePath("dos", "src/dos.zig");
     coff_exe.setBuildMode(mode);
-
-    // Old versions of setLinkerScriptPath take a string and new versions take a FileSource.
-    // See https://github.com/ziglang/zig/pull/7959.
-    const useFileSource = @typeInfo(@TypeOf(coff_exe.setLinkerScriptPath)).BoundFn.args[1].arg_type == FileSource;
-    const linkerScriptPath = "src/djcoff.ld";
-    const linkerScriptSource = if (useFileSource) FileSource.relative(linkerScriptPath) else linkerScriptPath;
-    coff_exe.setLinkerScriptPath(linkerScriptSource);
-
+    coff_exe.setLinkerScriptPath(FileSource.relative("src/djcoff.ld"));
     coff_exe.setTarget(try CrossTarget.parse(.{
         .arch_os_abi = "i386-other-none",
         .cpu_features = "_i386",
     }));
     coff_exe.single_threaded = true;
     coff_exe.strip = true;
-    const install_coff_exe = b.addInstallRaw(coff_exe, "demo.coff");
 
-    // Old value is .Bin and new value is .bin.
-    // See https://github.com/ziglang/zig/pull/7959.
-    const bin_dir: InstallDir = if (comptime std.meta.trait.hasField("bin")(InstallDir)) .bin else .Bin;
+    const install_coff_exe = b.addInstallRaw(coff_exe, "demo.coff");
 
     var cat_cmd = std.ArrayList(u8).init(b.allocator);
     defer cat_cmd.deinit();
@@ -41,19 +31,19 @@ pub fn build(b: *Builder) !void {
     // TODO: Host-neutral concatenation.
     try cat_cmd.writer().print("cat {s} {s} > {s}", .{
         b.pathFromRoot("deps/cwsdpmi/bin/CWSDSTUB.EXE"),
-        b.getInstallPath(bin_dir, "demo.coff"),
-        b.getInstallPath(bin_dir, "demo.exe"),
+        b.getInstallPath(.bin, "demo.coff"),
+        b.getInstallPath(.bin, "demo.exe"),
     });
 
     const add_stub = b.addSystemCommand(&[_][]const u8{
         "sh", "-c", cat_cmd.items,
     });
     add_stub.step.dependOn(&install_coff_exe.step);
-    b.pushInstalledFile(bin_dir, "demo.exe");
+    b.pushInstalledFile(.bin, "demo.exe");
     b.getInstallStep().dependOn(&add_stub.step);
 
     const run_in_dosbox = b.addSystemCommand(&[_][]const u8{
-        "dosbox", b.getInstallPath(bin_dir, "demo.exe"),
+        "dosbox", b.getInstallPath(.bin, "demo.exe"),
     });
     run_in_dosbox.step.dependOn(b.getInstallStep());
 
