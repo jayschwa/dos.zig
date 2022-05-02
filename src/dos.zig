777d2cbf1c4ece169d847b841537fb39c85abf3e
Jay Petacat <jay@jayschwa.net>
2022-05-02 16:52:56 -0400

    Remove unused `openFile` function
    This was originally used when the stub loader was written in Zig itself.

--- src/dos.zig
+++ src/dos.zig
@@ -1,6 +1,3 @@
-const std = @import("std");
-const File = std.fs.File;
-
 pub const dpmi = @import("dos/dpmi.zig");
 
 // Implement standard library operating system interfaces.
@@ -10,10 +7,4 @@ pub const system = @import("dos/system.zig");
 // file decides whether to export any appropriate start symbols.
 comptime {
     _ = @import("dos/start.zig");
-}
-
-// TODO: Integrate with standard library fs module.
-pub fn openFile(path: []const u8) !File {
-    const fd = try std.os.open(path, std.os.O_RDONLY, 0);
-    return File{ .handle = fd };
 }
